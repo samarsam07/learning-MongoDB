c71edfaabb0e1823a0137da6b56e987092f64608
samar <samarmohd251@gmail.com>
2024-12-30 20:54:36 +0530

    Implement database connection and add student collection operations in server.js

--- getting-started/server.js
+++ getting-started/server.js
@@ -21,6 +21,44 @@ const connectDB = async () => {
   try {
     await client.connect();
     console.log("connected to the database");
+    // create a new database
+    const database = client.db("student-database");
+    // new collection(student)
+    const students = database.collection("students");
+    //! new document using insertOne()
+    // const result = await students.insertOne({
+    //   name: "chenku",
+    //   age: 21,
+    //   subjects: ["maths", "science"],
+    // });
+    // console.log(result);
+    //! new document using insertMany()
+    // const result = await students.insertMany([
+    //   {
+    //     name: "bongu",
+    //     age: 22,
+    //     subjects: ["maths", "chemistry"],
+    //   },
+    //   {
+    //     name: "chotu",
+    //     age: 23,
+    //     subjects: ["english", "biology"],
+    //   },
+    //   {
+    //     name: "billa",
+    //     age: 18,
+    //     subjects: ["hindi", "history", "geography"],
+    //   },
+    // ]);
+    // console.log(result);
+    // !-------------------READ OPERATION-------------------
+    //? find() method
+    // const resultCursor=students.find();
+    // const resultArray=await resultCursor.toArray();
+    // console.log(resultArray);
+    // ? findOne() method
+    const result = await students.findOne({ age: 21 });
+    console.log(result);
   } catch (err) {
     console.log(err.message);
   }
