c7bfc192df7b8ab7dd9c80fae0059a756bdcff4f
samar <samarmohd251@gmail.com>
2025-01-04 21:28:14 +0530

    Add TypeScript definitions and configuration files for various node modules

---
+++ Validation/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 8082 || process.env.PORT;
+// connect to the database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://samarmohd251:fziRniuPfa4op4uX@learning.18puu.mongodb.net/student-database"
+    );
+    console.log("connected to the database");
+  } catch (err) {
+    console.log("error connecting to the database", err.message);
+  }
+};
+
+connectDB();
+// ! create a schema
+const userProfileSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+      min: 3,
+      max: 15,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      match: /@/,
+    },
+    age: {
+      type: Number,
+      required: [true, "Please provide an age"],
+      min: 18,
+      max: 100,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+      default: "Male",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+// model
+const User = mongoose.model("User", userProfileSchema);
+// create document
+const createDoc = async () => {
+  try {
+    const newUser = await User.create({
+      userName: "sher",
+      email: "sher123@gmail.com",
+      age: 24,
+      gender: "Male",
+    });
+    console.log(newUser);
+  } catch (e) {
+    console.log(e);
+  }
+};
+createDoc();//calling the function
+// start the server
+app.listen(port, () => {
+  console.log(`server is runnung on port ${port}`);
+});
