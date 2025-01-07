db936d623a1ad88f93d9f202d5fdbff72178a188
samar <samarmohd251@gmail.com>
2025-01-07 21:23:16 +0530

    Add cookie-parser middleware and implement user session management

--- Authentication/server.js
+++ Authentication/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 // middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 // set view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -23,15 +25,41 @@ app.get("/login", (req, res) => {
 });
 // login post route
 app.post("/login", (req, res) => {
+  // found user
   const { username, password } = req.body;
+  const userFound = users.find((user) => {
+    return user.username === username && user.password === password;
+  });
+  // create cutom cookie
+  res.cookie("userData", JSON.stringify(userFound), {
+    maxAge: 3 * 24 * 60 * 60 * 100, //3 days
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  });
+  // redirect to dashboard
+  if (userFound) {
+    res.redirect("/dashboard");
+  }
+  // redirect to login
 });
 // dashboard route
 app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+  // cookie
+  const userData = req.cookies.userData
+    ? JSON.parse(req.cookies.userData)
+    : null;
+  const username = userData ? userData.username : null;
+if(username){
+  res.render("dashboard",{username});
+}else{
+  res.redirect("/login");
+}
 });
 // logout route
-app.post("/logout", (req, res) => {
-  
+app.get("/logout", (req, res) => {
+  res.clearCookie("userData");
+  res.redirect("/login");
 });
 // start server
 app.listen(port, () => {
